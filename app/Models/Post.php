f90274dc31bbc2700fe0a579c6ce496684c72235
alphakahere <alphakahere@gmail.com>
2021-11-22 09:24:26 +0000

    show comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,8 @@ class Post extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,4 +11,27 @@
             {!!$post->body!!}
         </div>
     </div>
+    {{-- comment form --}}
+    <div class="w-full my-5 border-t border-gray-400 pt-5">
+        <form action="#" method="POST" class="w-full xl:w-2/3 ">
+            @csrf
+            <textarea name="content" id="content"  class=" border border-gray-400 outline-none rounded w-full p-2 text-gray-500" placeholder="Ajouter un commentaire"></textarea>
+            <button class="bg-blue-500 text-white px-2 py-1 rounded outline-none hover:bg-blue-500 transition-all ease-in duration-300">Commenter</button>
+        </form>
+    </div>
+    {{-- Comments --}}
+    @if (count($post->comments))
+        <section>
+            @foreach ($post->comments as $comment)
+                <div class="mb-5 flex space-x-2 items-center">
+                    <img src="{{asset('images/me.jpg')}}" alt="" class="w-10 h-10 rounded-full">
+                    <p>
+                        {{$comment->content}}
+                    </p>
+                </div>  
+            @endforeach 
+        </section>
+    @endif
+
+
 </x-layout>
